182881b2cf4ec3a9c2017a5582f48e048886e554
delin10 <23187420+delin10@users.noreply.github.com>
2018-05-28 20:56:54 +0800

    12312
    1213

---
+++ src/main/java/delin/javafx/manager/AppManager.java
@@ -0,0 +1,10 @@
+package delin.javafx.manager;
+
+import java.io.IOException;
+import javafx.stage.Stage;
+
+public interface AppManager {
+	public Object loadFXML(String fxml)throws IOException;
+	public void addStage(String id,Stage stage);
+	public Stage getStage(String id);
+}
